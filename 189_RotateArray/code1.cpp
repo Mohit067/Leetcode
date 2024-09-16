aaaa8db32046488dac91e330ba63304103cfae0b
Mohit067 <mohitsahu60067@gmail.com>
2024-09-16 13:40:58 +0530

    two sum added

---
+++ 189_RotateArray/code1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void reverse(int i, int j, vector<int>& v){
+        while(i<=j){
+            int temp = v[i];
+            v[i] = v[j];
+            v[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(k>n) k = k%n;
+
+
+        //logic is here
+        reverse(0, n-k-1, nums);
+        reverse(n-k, n-1, nums);
+        reverse(0, n-1, nums);
+    }
+};
