e419ac7c6d333ebf72b3fd7db9af8a0de10a0f64
Mohit067 <mohitsahu60067@gmail.com>
2024-09-17 11:56:46 +0530

    DutchAlgo added

---
+++ 75_sortColors/NormalAlgo.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int noz = 0;
+        int noo = 0;
+        int noT = 0;
+        
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == 0) noz++;
+            if(nums[i] == 1) noo++;
+            if(nums[i] == 2) noT++; 
+        }
+        for(int i=0; i<nums.size(); i++){
+            if(i<noz) nums[i] = 0;
+            else if(i<noo+noz) nums[i] = 1;
+            else nums[i] = 2;
+        }
+    }
+};
