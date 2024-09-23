78afb10760c927c9f521cc98a7635bf2ea588a2c
Mohit sahu <mohitsahu60067@gmail.com>
2024-09-23 13:05:38 +0530

    pascal triangle I added

---
+++ 118_Pascal_triangle/code.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+
+}
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        int n = numRows;
+        vector<vector<int>> v;
+        for(int i=1; i<=n; i++){
+            vector<int> a(i);
+            v.push_back(a);
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                if(j==0 || i==j){
+                    v[i][j] = 1;
+                }
+                else{
+                    v[i][j] = v[i-1][j] + v[i-1][j-1];
+                }
+            }
+        }
+        return v;
+    }
+};
