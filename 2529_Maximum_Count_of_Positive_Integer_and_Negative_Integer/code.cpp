83a3620f10731583cd0803e5ec99c5772405c8c6
Mohit sahu <mohitsahu60067@gmail.com>
2024-10-16 13:13:09 +0530

    2529 added

---
+++ 2529_Maximum_Count_of_Positive_Integer_and_Negative_Integer/code.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+#include<algorithm> 
+#include<math.h>
+using namespace std;
+int main(){}
+
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int posCount = 0;
+        int negCount = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == 0) continue;
+            else if(nums[i] > 0) posCount++;
+            else negCount++;
+        }
+        return max(posCount,negCount);
+    }
+};
+
