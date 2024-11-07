bdf3567f1657ee8c62969d429b4b5b6694a95ffa
Mohit sahu <mohitsahu60067@gmail.com>
2024-11-07 23:01:31 +0530

    217 added

---
+++ 217_Contain_Duplicate/code.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){}
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        int n = nums.size();
+        if(n==1) return false;
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<n-1; i++){
+            if(nums[i] == nums[i+1]) return true;
+        }
+        return false;
+    }
+};
