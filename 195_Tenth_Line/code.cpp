8cddfedf7b04b70be066fde7066ac423b6264bc0
Mohit sahu <mohitsahu60067@gmail.com>
2025-01-04 22:56:38 +0530

    195 added

---
+++ 195_Tenth_Line/code.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    
+    // awk 'NR == 10' file.txt; // we read 10th line using this linux command
+}
+
