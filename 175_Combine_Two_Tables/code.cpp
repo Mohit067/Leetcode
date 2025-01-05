f054a97fd47e241d58051fbe56cc0fc837e82597
Mohit sahu <mohitsahu60067@gmail.com>
2025-01-05 22:20:26 +0530

    175 added

---
+++ 175_Combine_Two_Tables/code.cpp
@@ -0,0 +1,10 @@
+// Write your MySQL query statement below
+//this is an query 
+// SELECT 
+//     Person.firstname, -- Select first name from Person table
+//     Person.lastname,  -- Select last name from Person table
+//     Address.city,     -- Select city from Address table
+//     Address.state     -- Select state from Address table
+// FROM Person
+// LEFT JOIN Address     -- Include all rows from Person, even if no match in Address
+// ON Person.personId = Address.personId; -- Match records based on personId
