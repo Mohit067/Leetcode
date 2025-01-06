ebb2e9b19af46002955c3d19cea6089955d88a10
Mohit sahu <mohitsahu60067@gmail.com>
2025-01-06 22:18:42 +0530

    182 added

---
+++ 182_Duplicate_Email/code.cpp
@@ -0,0 +1,6 @@
+// # Write your MySQL query statement below
+// SELECT Email            -- Select the email column
+// FROM Person
+// GROUP BY Email          -- Group rows by unique emails
+// HAVING COUNT(Email) > 1; -- Filter emails that appear more than once
+
